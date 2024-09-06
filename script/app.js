96ee79956e24c7c8253fa47d2922bc7116a2c32f
Ammar Ahmede <ammarahmed132004@outlook.com>
2024-09-07 02:29:35 +0300

    ED

--- script/app.js
+++ script/app.js
@@ -1,34 +1,3 @@
-// //
-
-// var toggleSwitch = document.querySelector(".toggle-button");
-
-// function switchTheme(e) {
-//   if (e.target.checked) {
-//     document.documentElement.setAttribute("data-theme", "dark");
-//     /*If required*/
-//     document.body.classList.add("dark-mode");
-//     document.body.classList.remove("light-mode");
-//   } else {
-//     document.documentElement.setAttribute("data-theme", "light");
-//     /*If required*/
-//     document.body.classList.add("light-mode");
-//     document.body.classList.remove("dark-mode");
-//   }
-// }
-
-// toggleSwitch.addEventListener("change", switchTheme, false);
-
-// //
-
-let loding = document.getElementById("loding");
-let _main = document.getElementById("_main");
-
-window.onload = function () {
-  loding.style.display = "none";
-
-  _main.style.display = "block";
-};
-
 function showDiv(id) {
   document.querySelectorAll(".content").forEach((div) => {
     div.style.display = "none";
@@ -93,41 +62,3 @@ function toggleAudio(audioId, playIconId, pauseIconId) {
     audioIcon.classList.remove("playing");
   }
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  var links = document.querySelectorAll("a.external-link");
-
-  links.forEach(function (link) {
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
-      var url = link.getAttribute("href");
-      window.open(url, "_blank");
-    });
-  });
-});
-
-// left_ptn
-
-let left_ptn = document.getElementById("left_ptn");
-let left_pt = document.getElementById("left_pt");
-
-let left_S = document.getElementById("left_S");
-
-left_ptn.addEventListener("click", function () {
-  left_S.style.left = "0";
-});
-
-left_pt.addEventListener("click", function () {
-  left_S.style.left = "1000vh";
-});
-
-let id = document.getElementById("id");
-let audio_left = document.getElementById("audio_left");
-let right_ = document.getElementById("right_");
-id.addEventListener("click", function () {
-  audio_left.style.left = "0";
-});
-
-right_.addEventListener("click", function () {
-  audio_left.style.left = "1000vh";
-});
